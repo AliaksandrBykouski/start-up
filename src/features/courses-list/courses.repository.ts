3163952c892e819ce9a97ab0032f69f0a7c1388c
Aliaksandr <157883894+AliaksandrBykouski@users.noreply.github.com>
2025-09-21 00:13:14 +0200

    form

---
+++ src/features/courses-list/courses.repository.ts
@@ -0,0 +1,30 @@
+import { cache } from 'react'
+
+import { dbClient } from '@/shared/lib/db'
+
+import {
+	CourseListElement,
+	CreateCourseListElementCommand,
+	DeleteCourseListElementCommand
+} from './model/types'
+
+class CoursesRepository {
+	getCoursesList = cache(
+		(): Promise<CourseListElement[]> => dbClient.course.findMany()
+	)
+
+	createCourseElement = (
+		command: CreateCourseListElementCommand
+	): Promise<CourseListElement> => {
+		return dbClient.course.create({
+			data: command
+		})
+	}
+	deleteCourseElement = (command: DeleteCourseListElementCommand) => {
+		return dbClient.course.delete({
+			where: { id: command.id }
+		})
+	}
+}
+
+export const coursesRepository = new CoursesRepository()
---
+++ src/features/courses-list/ui/course-item.tsx
@@ -0,0 +1,43 @@
+'use client'
+
+import { useTransition } from 'react'
+
+import { Button } from '@/shared/ui/button'
+import {
+	Card,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle
+} from '@/shared/ui/card'
+
+import { CourseListElement } from '../model/types'
+
+export function CourseItem({
+	course,
+	onDelete
+}: {
+	course: CourseListElement
+	onDelete: () => Promise<void>
+}) {
+	const [isLoadingDelete, startDeleteTransition] = useTransition()
+	const handleDelete = () => {
+		startDeleteTransition(async () => {
+			await onDelete()
+		})
+	}
+
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>{course.name}</CardTitle>
+				<CardDescription>{course.description}</CardDescription>
+			</CardHeader>
+			<CardFooter>
+				<Button disabled={isLoadingDelete} onClick={handleDelete}>
+					Удалить
+				</Button>
+			</CardFooter>
+		</Card>
+	)
+}
