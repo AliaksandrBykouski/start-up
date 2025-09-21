c2a8b5cbbec04d42388dc59ccf901e45343e9adb
Aliaksandr <157883894+AliaksandrBykouski@users.noreply.github.com>
2025-09-21 23:09:40 +0200

    fix

--- tests/courses-list.spec.ts
+++ tests/courses-list.spec.ts
@@ -3,8 +3,6 @@ import { expect, test } from '@playwright/test'
 test('create delete course list', async ({ page }) => {
 	await page.locator('body').click()
 	await page.goto('/')
-	await page.getByPlaceholder('название').click()
-	await page.getByPlaceholder('название').fill('Test course')
 	await page.getByPlaceholder('описание').click()
 	await page.getByPlaceholder('описание').fill('Test description')
 	await page.getByRole('button', { name: 'Добавить' }).click()
